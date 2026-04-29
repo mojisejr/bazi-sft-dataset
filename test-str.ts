d2fda7a057915e942423cdcd75e7505512896641
mojisejs <nonthasak.l@gmail.com>
2026-04-29 23:54:00 +0700

    chore(bazi): checkpoint before #oracle-bazi-ui-standardization phase2 ggg

--- test-str.ts
+++
@@ -1,2 +0,0 @@
-import { classifyOperatorStrengthScore } from "./src/lib/bazi/constants/operator-strength";
-console.log(classifyOperatorStrengthScore(4));
--- tmp_check2.ts
+++
@@ -1,9 +0,0 @@
-import { classifyOperatorStrengthScore } from "./src/lib/bazi/constants/operator-strength";
-import {
-	buildDayMasterStrengthVocabulary,
-	resolveCanonicalDayMasterStrengthState,
-} from "./src/lib/bazi/strength-state-vocabulary";
-
-console.log("Class for 4:", classifyOperatorStrengthScore(4));
-console.log("Vocabulary for 3.75:", buildDayMasterStrengthVocabulary(3.75));
-console.log("Canonical state for 6.5:", resolveCanonicalDayMasterStrengthState("6.5"));
